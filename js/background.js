4a4277bd7553dbac40753c741d23ffc24f146820
necdetyapici <53614445+necdetyapici@users.noreply.github.com>
2023-04-25 16:29:31 +0300

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,99 @@
+/**
+ * Created by neypc on 25.04.2023.
+ */
+
+var url = 'https://www.investingcurrencies.com?pairs=';
+var notification_levels = {};
+
+function notify(data, selected_currencies, notification_threshold) {
+    var notify = false;
+    var messages = [];
+
+    for (let curr_id of selected_currencies) {
+        if (!(curr_id in notification_levels)) {
+            //initialize level to 0
+            notification_levels[curr_id] = 0;
+        }
+        try {
+            var cur_percentage = data[curr_id]["percentage"];
+            var cur_name = data[curr_id]["name"];
+            var cur_rate = data[curr_id]["rate"];
+            if (!cur_rate || !cur_name || !cur_percentage) continue;
+
+            if (Math.abs(cur_percentage - notification_levels[curr_id]) > notification_threshold) {
+                notification_levels[curr_id] = cur_percentage;
+                notify = true;
+                messages.push(cur_name + " rate is " + cur_rate + " (%" + cur_percentage + ")");
+
+            }
+        }
+        catch(err) {
+            console.log(curr_id)
+            console.log(err.message);
+        }
+
+    }
+
+    if (notify === true) {
+        var options = {
+            type: 'basic',
+            title: 'Currency Rates Changes',
+            message: messages.join("\n"),
+            iconUrl: '../css/images/icon.jpeg',
+            requireInteraction: true
+        };
+        //console.log(messages);
+        chrome.notifications.create(options);
+
+    }
+
+}
+
+function check_rates() {
+    chrome.storage.sync.get({
+        selectedCurrencies: ["1", "2", "6"],
+        selectedCryptoCurrencies: ["945629", "1058142", "1158819"],
+        enableNotification: true,
+        notificationThreshold: 0.5,
+        notificationThresholdCrypto: 5
+    }, function (items) {
+        if (!items.enableNotification) {
+            return;
+        } else {
+            chrome.storage.session.get({notification_levels: {}}, function (result) {
+                notification_levels = result.notification_levels;
+            });
+            fetch(url + items.selectedCurrencies.concat(items.selectedCryptoCurrencies).join(","))
+                .then(function (response) {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' + response.status);
+                        return;
+                    }
+                    // The API call was successful!
+                    let data = response.json();
+                    data.then(function (result) {
+
+                        notify(result, items.selectedCurrencies, items.notificationThreshold)
+                        notify(result, items.selectedCryptoCurrencies, items.notificationThresholdCrypto)
+                        chrome.storage.session.set({notification_levels: notification_levels}, function () {
+                            console.log(notification_levels);
+                        });
+                    });
+                }).catch(function (err) {
+                // There was an error
+                console.log('Something went wrong.', err);
+            });
+        }
+
+    });
+}
+
+const periodInMinutes = 3;
+chrome.runtime.onInstalled.addListener(details => {
+    chrome.alarms.create("myAlarm", {periodInMinutes});
+});
+chrome.alarms.onAlarm.addListener((alarm) => {
+    //check_rates();
+    check_rates();
+});
+
---
+++ js/options.js
@@ -0,0 +1,149 @@
+// Saves options to chrome.storage
+function save_options() {
+    var curlist = $("#currency_select").select2("val");
+    var crypto_curlist = $("#crypto_currency_select").select2("val");
+    var language = $("#language").select2("val");
+    var active_tab = $("#active_tab").select2("val");
+    var enable_not = $("#enable_notification").prop("checked");
+    var threshold = parseFloat($("#notification_threshold").val());
+    var threshold_crypto = parseFloat($("#notification_threshold_crypto").val());
+    var status = $('#status');
+    if (threshold > 100 || threshold <= 0 || !threshold) {
+        status.text('Error: Threshold should be between 0 and 100.');
+        return;
+    }
+    if (threshold_crypto > 100 || threshold_crypto <= 0 || !threshold) {
+        status.text('Error: Threshold should be between 0 and 100.');
+        return;
+    }
+    if(curlist.length > 15 || crypto_curlist.length > 15){
+        status.text('Error: You can select maximum 15 currency.');
+        return;
+    }
+    chrome.storage.sync.set({
+        selectedCurrencies: curlist,
+        selectedCryptoCurrencies: crypto_curlist,
+        enableNotification: enable_not,
+        notificationThreshold: threshold,
+        notificationThresholdCrypto: threshold_crypto,
+        language: language,
+        active_tab: active_tab
+    }, function () {
+        // Update status to let user know options were saved.
+
+        status.text('Options saved. Window will be closed.');
+        setTimeout(function () {
+            window.close();
+        }, 2000);
+
+    });
+}
+moveElementToEndOfParent = function(element) {
+    let parent = element.parent();
+    element.detach();
+    parent.append(element);
+};
+
+sort_select_box = function (id, values) {
+    values.forEach(function (v){
+        let element = $("#"+id).children("option[value='"+v+"']");
+        moveElementToEndOfParent(element);
+    });
+
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    chrome.storage.sync.get({
+        selectedCurrencies: ["1", "2", "6"],
+        selectedCryptoCurrencies: ["945629", "1058142", "1158819"],
+        enableNotification: true,
+        notificationThreshold: 0.5,
+        notificationThresholdCrypto: 5,
+        language: "www",
+        active_tab: "currency"
+    }, function (items) {
+        sort_select_box("currency_select", items.selectedCurrencies);
+        $('#currency_select').val(items.selectedCurrencies).trigger('change');
+        sort_select_box("crypto_currency_select", items.selectedCryptoCurrencies);
+        $('#crypto_currency_select').val(items.selectedCryptoCurrencies).trigger('change');
+
+        $('#language').val(items.language).trigger('change');
+        $('#active_tab').val(items.active_tab).trigger('change');
+        if (items.enableNotification) {
+            $("#enable_notification").bootstrapToggle('on');
+        } else {
+            $("#enable_notification").bootstrapToggle('off');
+        }
+        $("#notification_threshold").val(items.notificationThreshold);
+        $("#notification_threshold_crypto").val(items.notificationThresholdCrypto);
+        let currency_select_elem = $('#currency_select');
+        currency_select_elem.select2({
+            placeholder: 'Select currencies'
+        }).on("select2:select", function (evt) {
+            let id = evt.params.data.id;
+            console.log(id);
+            let element = $(this).children("option[value="+id+"]");
+            moveElementToEndOfParent(element);
+            $(this).trigger("change");
+
+        });
+        let ele = currency_select_elem.parent().find("ul.select2-selection__rendered");
+        ele.sortable({
+            containment: 'parent',
+            cursor: "grabbing",
+            update: function() {
+                currency_select_elem.parent().find("ul.select2-selection__rendered").children("li[title]").each(function(i, obj){
+                    let element = $("#currency_select").children('option').filter(function () { return $(this).html() == obj.title });
+                    moveElementToEndOfParent(element)
+                });
+                console.log(""+currency_select_elem.val())
+            }
+        });
+
+        let crypto_currency_select_elem = $('#crypto_currency_select');
+        crypto_currency_select_elem.select2({
+            placeholder: 'Select crypto currencies'
+        }).on("select2:select", function (evt) {
+            let id = evt.params.data.id;
+            console.log(id);
+            let element = $(this).children("option[value="+id+"]");
+            moveElementToEndOfParent(element);
+            $(this).trigger("change");
+
+        });
+        let cele = crypto_currency_select_elem.parent().find("ul.select2-selection__rendered");
+        cele.sortable({
+            containment: 'parent',
+            cursor: "grabbing",
+            update: function() {
+                crypto_currency_select_elem.parent().find("ul.select2-selection__rendered").children("li[title]").each(function(i, obj){
+                    let element = $("#crypto_currency_select").children('option').filter(function () { return $(this).html() == obj.title });
+                    moveElementToEndOfParent(element)
+                });
+                console.log(""+crypto_currency_select_elem.val())
+            }
+        });
+
+        $('#language').select2({
+                minimumResultsForSearch: -1
+            }
+        );
+
+        $('#active_tab').select2({
+                minimumResultsForSearch: -1
+            }
+        );
+    });
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
+
+
+
